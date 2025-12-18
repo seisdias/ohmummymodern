d884bb0b2eeb8fbce5c8b0ac2dd84e0998fa865f
Borja Alonso Tristan <bor_jamon@hotmail.com>
2025-12-18 12:53:23 +0100

    fix tests

--- tests/test_reveal_cross.py
+++ tests/test_reveal_cross.py
@@ -2,6 +2,7 @@ from src.world.map import TileMap, CellContent
 from src.world.reveal import reveal_cross
 
 
+
 def test_reveal_cross_reveals_center_and_neighbors():
     m = TileMap.demo(10, 10, seed=1)
 
@@ -15,14 +16,25 @@ def test_reveal_cross_reveals_center_and_neighbors():
     assert m.is_revealed(cx, cy - 1)
 
 
+
+
 def test_reveal_cross_scores_treasures_only_once():
     m = TileMap.demo(10, 10, seed=1)
 
-    # Forzamos un tesoro conocido
-    m.contents[5][5] = CellContent.TREASURE
+    cx, cy = 5, 5
+    cross = [(cx, cy), (cx+1, cy), (cx-1, cy), (cx, cy+1), (cx, cy-1)]
 
-    score1 = reveal_cross(m, 5, 5)
-    score2 = reveal_cross(m, 5, 5)
+    # Aseguramos determinismo: vaciamos la cruz
+    for x, y in cross:
+        m.contents[y][x] = CellContent.EMPTY
+        m.revealed[y][x] = False
+
+    # Forzamos UN tesoro conocido en el centro
+    m.contents[cy][cx] = CellContent.TREASURE
+
+    score1 = reveal_cross(m, cx, cy)
+    score2 = reveal_cross(m, cx, cy)
 
     assert score1 == 100
     assert score2 == 0
+
